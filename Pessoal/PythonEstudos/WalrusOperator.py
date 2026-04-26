ece6c97cc861c8742fbe00eb9d8024238afc2d90
zcat-cat <rafaeldemboski2@gamil.com>
2026-04-25 23:08:07 -0300

    walrus

--- Pessoal/PythonEstudos/WalrusOperator.py
+++ Pessoal/PythonEstudos/WalrusOperator.py
@@ -1,3 +1,14 @@
+Sair = ""
+
+while Sair != 'n':
+    Sair = input("Digite n para sair")
+
+    if not Sair == 'n':
+        print("Não saiu.")
+
+print("Saiu!")
+
+
 
 while (sair := input("Digite n para sair: ")) != 'n':
     print("Não saiu.")
